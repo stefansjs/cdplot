88e914b735c162a993fededc411128ec5bb5cb6b
Stefan Sullivan <stefan.sullivan@gmail.com>
2023-12-24 13:38:48 -0800

    cleanup data.py

--- python/plot_torque_pro/data.py
+++ python/plot_torque_pro/data.py
@@ -8,26 +8,10 @@ from collections import defaultdict
 from pathlib import Path
 
 import pandas
-import plotly.express
 
-from .config import determine_columns, serialize_config
+from .config import determine_columns
 
 logger = logging.getLogger(__name__)
-
-
-def plot_data(csv_path: Path, config: dict):
-    csv_data = load_from_csv(csv_path, config)
-    plot_handle = render_plot(csv_data, config['plot'])
-
-    logger.debug("To reproduce this plot, put the following toml into its own config file\n%s",
-                 serialize_config(config))
-
-    if config.get('output_path'):
-        plot_handle.write_to_file(config['output_path'])
-    else:
-        plot_handle.show()
-
-    logger.info("done")
 
 
 def load_from_csv(csv_path, config):
@@ -61,10 +45,6 @@ def load_from_csv(csv_path, config):
     csv_dataframe = csv_dataframe[plot_columns].copy()
 
     return csv_dataframe
-
-
-def render_plot(csv_data, config):
-    return plotly.express.line(csv_data, x=config['x'], y=config['y'])
 
 
 def preprocess_data(csv_path):
