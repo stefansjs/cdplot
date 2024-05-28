3a725b6e8f655fda8b27b1f6d5debe1c4b51137c
Stefan Sullivan <stefan.sullivan@gmail.com>
2024-05-27 22:40:37 -0700

    bump version to 1.0a

--- python/plot_torque_pro/__init__.py
+++ python/plot_torque_pro/__init__.py
@@ -3,19 +3,9 @@ Package for plot_torque_pro
 """
 
 
+__version__ = "1.0a"
+
+
 def get_version():
     """ Returns the package and build version of plot_torque_pro """
-    from pathlib import Path
-    package_directory = Path(__file__).parent
-    version_file = package_directory / 'VERSION'
-
-    if version_file.exists():
-        import toml
-        version_info = toml.load(version_file)
-        return version_info['__version__']
-
-    from datetime import date
-    return f'UNKNOWN+{date.today().strftime("%Y.%m.%d")}'
-
-
-__version__ = get_version()
+    return __version__
